d85dd94eadd86846a89f25699f9303e24e72bfec
Josue Bravo <51432482+neonixran@users.noreply.github.com>
2025-04-23 17:34:54 -0400

    Hola mundo

--- Ejercicios/Ejercicio 1.py
+++ Ejercicios/Ejercicio 1.py
@@ -6,7 +6,6 @@ resultado
 print("Este programa suma 2 números")
 num1 = int(input("Ingrese el primer número: "))
 num2 = int(input("Ingrese el segundo número: "))
-sum = 0
 
 sum = num1 + num2
-print(f"El resultado de la suma es {sum}")
+print(f"El resultado de {num1} + {num2} es {sum}")
