e68660ec9b04c8773a433ee6812eb4b91c0163b1
Josue Bravo <51432482+neonixran@users.noreply.github.com>
2025-05-06 18:06:48 -0400

    uwu

---
+++ Clases/Clase 3/Ejercicio 1.py
@@ -0,0 +1,40 @@
+print("Este programa calcula el IMC (Índice de Masa Corporal)")
+
+rep = "s" # "s": Continua ejecutandose, "n": Termina el programa (puede ocurrir que con cualquier letra termina)
+
+# Comprueba que mientras la variable rep sea igual a "s" se siga ejecutando
+while rep.lower() == "s":
+    masa = float(input("Ingrese su peso (kg): "))
+    estatura = float(input("Ingrese su estatura (cm): "))
+
+    # Comprobar que el peso (masa) y estatura estén dentro del rango permitido
+    while (masa <= 0 or masa > 500) or (estatura <= 0 or estatura > 250):
+        print("[!] Debe ingresar valores válidos.")
+        masa = float(input("Ingrese su peso (kg): "))
+        estatura = float(input("Ingrese su estatura (cm): "))
+
+    # Cálculo del imc, conversión de centímetros a metros
+    imc = masa / ((estatura / 100) ** 2)
+
+    # Tipos de peso
+    if imc < 18:
+        print("Bajo peso")
+    elif imc < 25:
+        print("Peso normal")
+    elif imc < 30:
+        print("Exceso de peso")
+    elif imc < 35:
+        print("Obesidad grado I")
+    elif imc < 40:
+        print("Obesidad grado II")
+    elif imc < 45:
+        print("Obesidad grado III (mórbida)")
+    elif imc > 50:
+        print("Obesidad grado IV (extrema)")
+    else:
+        print("No fue posible calcular")
+
+    print(f"Su IMC es: {imc:.1f}")
+
+    rep = input("\n¿Quiere calcular de nuevo? (s/n): ")
+    print("¡Hasta luego!")
--- Evaluaciones/Test 3 (30-04-2025)/Ejercicio 2.py
+++ Evaluaciones/Test 3 (30-04-2025)/Ejercicio 2.py
@@ -1,4 +1,3 @@
-#30/04/2025
 # Programa que calcule e indique cuantas semanas habrá vivido cuando esté de cumpleaños. Considerar que son 52 semanas por año.
 print("Este programa calcula cuantas semanas habrá vivido cuando esté de cumpleaños")
 nacimiento = int(input("Ingrese su año de nacimiento: ")) # Año de nacimiento.
