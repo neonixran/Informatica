fd0c87000c9924b4ffc56116515dde21c745e75a
Josue Bravo <51432482+neonixran@users.noreply.github.com>
2025-05-08 23:23:24 -0400

    #

--- Clases/Clase 4 (07-05)/Uso de bucle While.py
+++ Clases/Clase 4 (07-05)/Uso de bucle While.py
@@ -1,6 +1,6 @@
 n = float(input("Ingrese un número positivo: "))
 
-while n < 0:
+while n < 0: # Comprueba si el el número ingresado es menor a 0, de serlo vuelve a preguntarle al usuario.
     print("Ingrese número positivo")
     n = float(input("Ingrese un número positivo"))
 
