816d9bd8f6ecfc49c4637f37ae65149a5bddffae
Josue Bravo <51432482+neonixran@users.noreply.github.com>
2025-05-14 14:00:13 -0400

    uwu

--- Clases/Clase 5 (13-05)/Ejemplo 1.py
+++ Clases/Clase 5 (13-05)/Ejemplo 1.py
@@ -4,7 +4,7 @@ for i in range(1, 4):
         try:
             n1 = float(input("Ingrese nota 1: "))
 
-            if 1 < n1 > 7:
+            if n1 < 1 or n1 > 7: # 1 < n1 > 7
                 print("Ingrese una nota dentro del rango (1.0 a 7.0)")
                 continue
         except ValueError:
@@ -14,7 +14,7 @@ for i in range(1, 4):
         try:
             n2 = float(input("Ingrese nota 2: "))
 
-            if 1 < n2 > 7:
+            if n2 < 1 or n2 > 7: # 1 < n2 > 7
                 print("Ingrese una nota dentro del rango (1.0 a 7.0)")
                 continue
         except ValueError:
