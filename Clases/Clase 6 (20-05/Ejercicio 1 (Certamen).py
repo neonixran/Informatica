18a745e1363031069313faa75875ebd78c34ce2f
Josue Bravo <51432482+neonixran@users.noreply.github.com>
2025-05-20 22:08:28 -0400

    - Ejercicicios de la clase de hoy - Cambios en el código - Juego - Bla bla

---
+++ Clases/Clase 6 (20-05/Ejercicio 1 (Certamen).py
@@ -0,0 +1,16 @@
+r = "s"
+
+while r == "s":
+    a = int(input("Ingrese el procentaje de asistencia a clases: "))
+
+    if a >= 0 and a <= 100:
+        print(f"Su porcentaje de asistencia es {a} y es válido")
+    else:
+        print("Ingrese porcentaje válido")
+
+    r = input("Otra vez (s/n): ").lower()
+
+    # Comprueba si la variable "r" es distinta de "s" o "n", útil para que solo pueda ingresar esos dos valores y no otros.
+    while r != "n" and r != "s":
+        print("Ingrese un valor válido")
+        r = input("Otra vez (s/n): ").lower()
--- Personal/Listado/Condicionales/Ejercicio 5.py
+++ Personal/Listado/Condicionales/Ejercicio 5.py
@@ -4,10 +4,43 @@ superiores a 1000 € mensuales. Escribir un programa que pregunte al usuario su
 mensuales y muestre por pantalla si el usuario tiene que tributar o no.
 '''
 
-edad = int(input("Ingrese su edad: "))
-ingresos_mensuales = float(input("Ingrese sus ingresos mensuales: "))
+continuar = "s"
 
-if edad > 16 and ingresos_mensuales >= 1000:
-    print("Debe tributar")
-else:
-    print("No debe tributar")
+while continuar == "s":
+    while True:
+        try:
+            edad = int(input("Ingrese su edad: "))
+
+            if edad <= 0:
+                print("\t[!] Debe ingresar un edad válida")
+                continue
+            break
+        except ValueError:
+            print("Ingrese sólo números")
+    
+    while True:
+        try:
+            ingresos_mensuales = float(input("Ingrese sus ingresos mensuales: "))
+
+            if ingresos_mensuales <= 0:
+                print("\t[!] Debe ingresar un ingreso mensual válido")
+                continue
+            break
+        except ValueError:
+            print("Ingrese sólo números")
+
+    if edad > 16 and ingresos_mensuales >= 1000:
+        print("Debe tributar")
+    else:
+        print("No debe tributar")
+    
+    while True:
+        try:
+            continuar = input("Quiere volver a ejecutar? (s/n): ").lower()
+
+            if continuar != "s" and continuar != "n":
+                print("\t[!] Debe ingresar un valor válido")
+                continue
+            break
+        except ValueError:
+            print("Ingrese solo caracteres")
