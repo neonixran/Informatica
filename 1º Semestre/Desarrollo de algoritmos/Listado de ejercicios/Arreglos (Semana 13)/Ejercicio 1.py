4347bf7bb71a7a91f8d3cc6b33ce5f20a55cbb77
Josue Bravo <51432482+neonixran@users.noreply.github.com>
2025-06-07 00:32:32 -0400

    🛌🏻

--- 1º Semestre/Desarrollo de algoritmos/Listado de ejercicios/Arreglos (Semana 13)/Ejercicio 1.py
+++ 1º Semestre/Desarrollo de algoritmos/Listado de ejercicios/Arreglos (Semana 13)/Ejercicio 1.py
@@ -3,18 +3,14 @@ Hacer un programa que lea 10 elementos, los guarde en un arreglo, luego duplique
 elementos del arreglo (que queden guardados) e imprima los elementos duplicados
 '''
 
-longitud = 10
+longitud = 2
 A = []
 
 for i in range(longitud): # El usuario ingresa la cantidad de números definidos por la variable longitud
     dato = int(input(f"Ingrese el elemento 1.{i+1}: ")) # Variable de paso para ingresar los números
     A.append(dato)
 
-print()
-print(f"Normal: {", ".join([str(A[i]) for i in range(longitud)])}") # Imprime el arreglo original con los números ingresados por el usuario
-
-
 for i in range(longitud):
-    A[i] *= 2 # Toma los valores del arreglo A y los duplica
+    A[i] *= 2
 
 print(f"Duplicada: {", ".join([str(A[i]) for i in range(longitud)])}") # Imprime el mismo arreglo pero ahora con los números duplicados
