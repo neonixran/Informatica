deb05dcd66e11adee86094bcb7f37e8a9b65fc47
Josue Bravo <51432482+neonixran@users.noreply.github.com>
2025-09-10 09:21:06 -0300

    uwu

--- 2º Semestre/Interfaz de Usuario/Personal/uwu/uwu/obj/Debug/net8.0-windows/uwu.AssemblyInfo.cs
+++ 2º Semestre/Interfaz de Usuario/Personal/uwu/uwu/obj/Debug/net8.0-windows/uwu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("uwu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae8fd3a2f1bc11848f99661aed02a343cddba8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adc2672cc00bb3dc1e819941d26e321c1494844f")]
 [assembly: System.Reflection.AssemblyProductAttribute("uwu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("uwu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
