04c432b2d2e44b7a72f03f7f5b9cce566f8cee9f
Josue Bravo <51432482+neonixran@users.noreply.github.com>
2025-10-24 19:12:45 -0300

    mish

---
+++ 2º Semestre/Taller de Programación/Unidades/Unidad 5/slnUnidad5/pryPráctico5/frmPráctico5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryPráctico5
+{
+    public partial class frmPráctico5 : Form
+    {
+        public frmPráctico5()
+        {
+            InitializeComponent();
+        }
+        static bool ValidaCorreo(string email)
+        {
+            if (email.Contains("@") && email.Contains(".") && !String.IsNullOrWhiteSpace(email) && !email.Contains("ñ")) 
+            {
+                foreach (char c in email)
+                {
+                    if (!char.IsUpper(c))
+                    {
+                        return true;
+                    } else
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            return false;
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            if (ValidaCorreo(txtEmail.Text))
+            {
+                MessageBox.Show("Email OK", "BIEN");
+            }
+            else
+                MessageBox.Show("Email incorrecto", "MAL");
+        }
+    }
+}
